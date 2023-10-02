4a3166a41a4bf094d548123d3b16e1e0f4a72eb9
Yash Agarwal <yash.ag@outlook.com>
2023-10-02 13:25:27 +0530

    added navbar

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+import Theme from "@/components/shared/navbar/Theme";
+import MobileNav from "@/components/shared/navbar/MobileNav";
+import GlobalSearch from "@/components/shared/search/GlobalSearch";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6  shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="DevOverflow"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 mx-sm:hidden">
+          Dev <span className="text-primary-500">Overflow</span>
+        </p>
+      </Link>
+      <GlobalSearch />
+      <div className="flex-between gap-5">
+        <Theme />
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
