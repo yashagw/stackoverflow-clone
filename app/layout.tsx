cc2a4ea0ce1d87160734b883ac91980300ac7616
Yash Agarwal <yash.ag@outlook.com>
2023-10-15 03:54:14 +0530

    Added Question Detail Page, Answer Form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "@/styles/prism.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
